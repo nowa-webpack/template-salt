9d65c01fa916bf0bf8edb3908a9569798dc92bd0
wenzhao.fw <wenzhao.fw@alibaba-inc.com>
2017-05-23 14:18:15 +0800

    fix nowa build bug

--- proj/webpack.config.js
+++ proj/webpack.config.js
@@ -1,27 +1,16 @@
-
-
 module.exports = function webpackConfig(config, webpack) {
   config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
     name: 'app',
     minChunks: Infinity,
   }));
 
-  if (process.argv[2] === 'server') {
-    config.externals = [{
-      lie: 'window.Promise',
-      react: 'window.React',
-      'react-dom': 'window.ReactDOM || window.React',
-      'react-router': 'window.ReactRouter',
-      fastclick: 'window.FastClick',
-      lodash: 'window._',
-    },
-    ];
-  } else {
-    delete config.externals;
-    config.module.loaders.forEach((n) => {
-      if (/\.css/.test(n.test)) {
-        delete n.include;
-      }
-    });
-  }
+  config.externals = [{
+    lie: 'window.Promise',
+    react: 'window.React',
+    'react-dom': 'window.ReactDOM || window.React',
+    'react-router': 'window.ReactRouter',
+    fastclick: 'window.FastClick',
+    lodash: 'window._',
+  },
+  ];
 };
